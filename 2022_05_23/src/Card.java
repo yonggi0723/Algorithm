35bb4cac4ab81e223cbce5496946d01971a02849
kim-yong-gi <kim-yong-gi@218.38.137.27>
2023-02-06 11:05:41 +0900

    update

--- 2022_05_23/src/Card.java
+++ 2022_05_23/src/Card.java
@@ -7,13 +7,36 @@ import java.util.*;
 class Card {
 
     public int solution(int[][] board, int r, int c) {
-        int answer = 0;
+        int answer = 0, v = 0;
         
+        boolean[] table = new boolean[7];
         
+        for (int i=0; i<board.length; i++)
+        	for (int j=0; j<board[i].length; j++) {
+        		v = board[i][j];
+        		
+        		if (v != 0 )
+        			table[v] = true;
+        		answer++;
+        	}
 
+        
+        per(0, answer/2, table);
+        
         return answer;
     }
+    
+    private void per( int depth, int count, boolean[] table) {
+    	if (depth == count) 
+    		return;
 
+    	for (int i=0; i<table.length; i++)
+    		if ( table[i] == true) {
+    			table[i] = false;
+    			per(depth+1, count, table);
+    			table[i] = true;
+    		}
+    }
     
 	@Test
 	void test() {
