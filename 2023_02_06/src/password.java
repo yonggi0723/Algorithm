37553646d38842ff76e48f723bc2a8201620068d
kim-yong-gi <kim-yong-gi@218.38.137.27>
2023-02-06 17:24:38 +0900

    update

--- 2023_02_06/src/password.java
+++ 2023_02_06/src/password.java
@@ -14,14 +14,25 @@ import org.junit.jupiter.api.Test;
 class password {
 	
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+        	int idx = index;
+        	char tmp = ch;
+        	
+        	for (int i=0; i<idx; i++) {
+        		tmp = tmp >= 'z' ? 'a':(char) (tmp + 1);
+        		idx += skip.contains(String.valueOf(tmp)) ? 1:0;
+        	}
+
+        	answer.append(tmp);
+        }
+        
+        return answer.toString();
     }
     
-
+    
 	@Test
 	void test() {
 		Assert.assertEquals("hello", solution("dziil", "ascf", 3));
-
 	}
 }
